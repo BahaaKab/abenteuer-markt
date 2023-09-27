ce952ce0d0ec2f87355b3f40e41f005da0203a83
bahaakabaweh <bahaakabaweh@gmail.com>
2023-09-28 02:17:02 +0400

    init material ui

---
+++ abenteuer-markt/src/pages/create-adventure/create-adventure.tsx
@@ -0,0 +1,18 @@
+import {Button, Typography} from '@mui/material';
+
+const CreateAdventure=()=>{
+    return(
+        <>
+            <p>
+                create adventure apge
+            </p>
+            <Button variant="contained" color='secondary'>2ere b wael</Button>
+
+
+            <Typography > 2ere be wael </Typography>
+
+        </>
+    )
+}
+
+export default CreateAdventure
